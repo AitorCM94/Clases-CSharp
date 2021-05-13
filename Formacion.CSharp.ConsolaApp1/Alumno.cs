6a09de08a334f5613c193c5ff3e23bade5627fa8
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-05-13 23:41:41 +0200

    PrimerCommit

---
+++ Formacion.CSharp.ConsolaApp1/Alumno.cs
@@ -0,0 +1,9 @@
+﻿namespace Formacion.CSharp.Objects
+{
+    public class Alumno
+    {
+        string Nombre = "Aitor";
+        string Apellidos = "Cerdán";
+        public int Edad = 26;
+    }
+}
--- Formacion.CSharp.ConsolaApp1/Program.cs
+++ Formacion.CSharp.ConsolaApp1/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System; //import -> using. System es el Espacio de Nombre.
 using Formacion.CSharp.ConsolaApp1;
 
-namespace Formacion.CSharp.ConsolaApp1
+namespace Formacion.CSharp.ConsolaApp1 //Espacio de Nombres -> Contiene las diferentes clases/objetos.
 {
-    class Program
+    class Program //Clase inicial. Siempre trabajamos con clases.
     {
-        static void Main(string[] args) //Inicio del programa.
+        static void Main(string[] args) //Método (función) estático -> Donde se inicia el programa.
         {
             Objects.Alumno alumno = new Objects.Alumno(); //Instanciar el objeto.
 
