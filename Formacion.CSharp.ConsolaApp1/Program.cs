f8896b77068260402f0b635a6840a6f72cfba97b
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-05-13 15:18:56 +0200

    Add project files.

---
+++ Formacion.CSharp.ConsolaApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Formacion.CSharp.ConsolaApp1;
+
+namespace Formacion.CSharp.ConsolaApp1
+{
+    class Program
+    {
+        static void Main(string[] args) //Inicio del programa.
+        {
+            Objects.Alumno alumno = new Objects.Alumno(); //Instanciar el objeto.
+
+            Console.WriteLine("Edad: {0}", alumno.Edad);
+        }
+    }
+}
+
+namespace Formacion.CSharp.Objects
+{
+    public class Alumno
+    {
+        string Nombre = "Aitor";
+        string Apellidos = "Cerdán";
+        public int Edad = 26;
+    }
+}
