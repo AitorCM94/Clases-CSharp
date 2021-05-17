b2c86660e53f82e478432430cacaa3de5e487adf
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-05-17 18:02:30 +0200

    fiClase2

--- Formacion.CSharp.ConsolaApp1/02Instanciar.cs
+++ Formacion.CSharp.ConsolaApp1/02Instanciar.cs
@@ -7,7 +7,129 @@ namespace Formacion.CSharp.ConsoleApp1
     {
         static void Main(string[] args) //Comienzo de la aplicación.
         {
-            //DIFERENTES FORMAS DE DECLARAR UNA VARIABLE DE TIPO OBJETO + ASIGNACIONES:
+            //Recorrer array con un while y do while.
+            string[] fruta2 = {"naranja", "limón", "pomelo", "lima", "fresa", "sandía", "melón" };
+            int posicion = 0;
+            while (posicion < fruta2.Length)
+            {
+                Console.WriteLine(fruta2[posicion]);
+                posicion += 1;
+            }
+            int posicion2 = 0;
+            do
+            {
+                Console.WriteLine(fruta2[posicion2]);
+                posicion2 += 1;
+            } while (posicion2 < fruta2.Length);
+
+
+            Console.ReadKey();
+            //Preguntamos un número
+            Console.Write("Número: ");
+            string input = Console.ReadLine();
+            int numero = Convert.ToInt32(input);
+
+            for (int i = 1; i < 11; i++)
+                Console.WriteLine(i * numero);
+
+
+            //Pintamos la tabla de multiplicar
+
+
+            
+            decimal[] numeros3 = { 10, 5, 345, 55, 13, 1000, 83 };
+            //Suma total
+            decimal total = 0;
+            for (int i = 0; i < numeros3.Length; i++)
+            {
+                total += numeros3[i];
+            }
+            int total2 = 0;
+            foreach (int num in numeros3)
+            {
+                total2 += num;
+            }
+
+            //Media -> Dividir el total entre len
+            decimal media = total / numeros3.Length;
+            int media2 = total2 / numeros3.Length;
+
+            //Max y min
+            decimal max = 0;
+            decimal min = numeros3[0];
+            for (int i = 0; i < numeros3.Length; i++)
+            {
+                if (numeros3[i] > max) max = numeros3[i];
+                if (numeros3[i] < min) min = numeros3[i];
+            }
+
+            
+            Reserva reserva = new Reserva();
+
+            Console.Write("ID de la Reserva: ");
+            reserva.id = Console.ReadLine();
+
+            Console.Write("Nombre del Cliente: ");
+            reserva.cliente = Console.ReadLine();
+
+            // 100: Habitación Individual  200: Habitación Doble  300: Junior Suite  400: Suite
+            Console.Write("Tipo de Reserva: ");
+            //reserva.tipo = Convert.ToInt32(Console.ReadLine());
+            string respuesta = Console.ReadLine();
+            int.TryParse(respuesta, out reserva.tipo);
+
+            Console.Write("Es Fumador ? ");
+            //reserva.fumador = Convert.ToBoolean(Console.ReadLine());
+            string respuesta2 = Console.ReadLine();
+            //if (respuesta2.ToLower().Trim() == "si" || respuesta2.ToLower().Trim() == "sí") reserva.fumador = true; else reserva.fumador = false;
+            //reserva.fumador = (respuesta2.ToLower().Trim() == "si" || respuesta2.ToLower().Trim() == "sí") ? true : false;
+            switch (respuesta2.ToLower().Trim())
+            {
+                case "si":
+                    reserva.fumador = true;
+                    break;
+                case "sí":
+                    reserva.fumador = true;
+                    break;
+                default:
+                    reserva.fumador = false;
+                    break;
+            }
+
+            Console.Clear();
+
+            //Pinta el Número de la Reserva:
+            Console.WriteLine($"ID de la Reserva: {reserva.id}");
+
+            //Pinta Nombre del Cliente:
+            Console.WriteLine($"Nombre del Cliente: {reserva.cliente}");
+
+            //Pinta el tipo de reserva en texto:
+            if (reserva.tipo == 100)
+                Console.WriteLine("Habitación individual.");
+            else if (reserva.tipo == 200)
+                Console.WriteLine("Habitación doble.");
+            else if (reserva.tipo == 300)
+                Console.WriteLine("Junior Suite");
+            else if (reserva.tipo == 400)
+                Console.WriteLine("Suite.");
+            else
+                Console.WriteLine("No es correcto.");
+            
+            //Pinta si es fumador en texto:
+            switch(reserva.fumador)
+            {
+                case true:
+                    Console.WriteLine("Es fumador.");
+                    break;
+                case false:
+                    Console.WriteLine("No es fumador.");
+                    break;
+            }
+            
+
+            Console.ReadKey();
+            //DIFERENTES FORMAS DE DECLARAR UNA VARIABLE DE TIPO OBJETO. Y ASIGNACIONES:
             Alumno alumno1 = new Alumno(); //Especificando la clase -> ALUMNO. (Valor por defecto -> Null)
             var alumno2 = new Alumno(); //En el momento de la asignación toma tipo.
 
@@ -70,7 +192,7 @@ namespace Formacion.CSharp.ConsoleApp1
                 Apellidos = "Berta" //, para añadir más variables a modificar
             };
 
-            Console.ReadKey(); //Para que se quede parado esperando que pulsemos una tecla
+            //Console.ReadKey(); //Para que se quede parado esperando que pulsemos una tecla
         }
     }
 }
@@ -82,5 +204,23 @@ namespace Formacion.CSharp.Objects
         string Nombre = "Aitor"; //Creación de variables que contienen alfanumericos.
         public string Apellidos = "Cerdán"; //Hacemos la variable pública
         int Edad = 26; //Creación de variables que contienen numéricos.
+    }
+
+    public class Reserva
+
+    {
+
+        public string id;
+
+        public string cliente;
+
+
+
+        // 100: Habitación Individual  200: Habitación Doble  300: Junior Suite  400: Suite
+
+        public int tipo;
+
+        public bool fumador;
+
     }
 }
