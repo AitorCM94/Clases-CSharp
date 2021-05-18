38590e896fdaae703465c922d3b3496c8227d820
Aitor Cerdán Mañé <82872545+AitorCM94@users.noreply.github.com>
2021-05-19 00:14:57 +0200

    WebApp

---
+++ 02 Creating Methods, Handling Exceptions and Monitoring Applications/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Creating_Methods__Handling_Exceptions_and_Monitoring_Applications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller // Classe HomeController que hereda de Controller. [hijo] : [padre]
+    {
+        public IActionResult Index(int id) //Método Index que retorna IActionResult (tipo de objeto interfaz)
+        {
+            //Traspasamos información a la vista utilizada ViewBag
+            ViewBag.numero = id;
+            ViewBag.mensaje = $"Tabla de Multiplicar del {id}";
+
+            //Traspasamos información como modelo de datos
+            return View(id); //Todos los métodos tienen que estar asociados a una vista.
+        }
+
+        public IActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
